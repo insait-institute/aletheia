1f259c164f35f8b548d92d6fd9e5b0a627d0d9ad
vatsal-kr <57312637+vatsal-kr@users.noreply.github.com>
2025-04-30 13:10:27 +0000

    update default config

--- src/configs/sft_coldstart_config.py
+++ src/configs/sft_coldstart_config.py
@@ -7,7 +7,7 @@ from hydra.core.config_store import ConfigStore
 class Data:
     path: str = "CodeShield/cold_start_data"
     split_ratio: float = 0.9
-    subset: int | float = 10000
+    subset: int | float = 1.0
 
 
 @dataclass
