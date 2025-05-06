9069ea25cecba111a4e695a9d2e2c046b5a3e094
vatsal-kr <57312637+vatsal-kr@users.noreply.github.com>
2025-05-06 13:44:17 +0000

    Create rm sft scripts

---
+++ src/configs/rm_training_config.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass, field
+
+from hydra.core.config_store import ConfigStore
+
+
+@dataclass
+class Data:
+    path: str = "CodeShield/GenRM_data_0.33"
+    split_ratio: float = 1.0
+
+
+@dataclass
+class SFTParams:
+    model_name: str = "Qwen/Qwen2.5-7B-Instruct"
+    batch_size: int = 4
+    num_epochs: int = 3
+    learning_rate: float = 1e-4
+    weight_decay: float = 1e-2
+    warmup_steps: int = 0
+    logging_steps: int = 1
+    max_length: int = 4096
+    tp_size: int = 0
+    lr_scheduler_type: str = "cosine_with_min_lr"
+    lr_scheduler_kwargs: dict = field(default_factory=lambda: {"min_lr": 1e-5})
+    gradient_accumulation_steps: int = 16
+    overwrite_output_dir: bool = True
+    seed: int = 42
+    use_bf16: bool = True
+    resume_training_if_possible: bool = True
+    deepspeed_config_path: str = "configs/deepspeed_config.json"
+
+
+@dataclass
+class WandbParams:
+    run_name: str = "rm_training_test_run"
+    log_level: str = "info"
+
+
+@dataclass
+class Config:
+    data: Data = field(default_factory=Data)
+    sft_params: SFTParams = field(default_factory=SFTParams)
+    wandb_params: WandbParams = field(default_factory=WandbParams)
+
+
+cs = ConfigStore.instance()
+cs.store(name="rm_training_config", node=Config)
