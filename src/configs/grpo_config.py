5c900b3597239491e271146f7b0a025116bc4433
vatsal-kr <57312637+vatsal-kr@users.noreply.github.com>
2025-06-09 12:33:40 +0000

    Add separate deepspeed configs for zero stages

--- src/configs/grpo_config.py
+++ src/configs/grpo_config.py
@@ -14,11 +14,11 @@ class GRPOParams:
     batch_size: int = 4
     beta: float = 1e-3
     epsilon: float = 0.2
+    epsilon_high: float = 0.4
     eval_batch_size: int = 16
     gradient_accumulation_steps: int = 32
     learning_rate: float = 1e-6
     logging_steps: int = 1
-    loss_type: str = "dr_grpo"
     lr_scheduler_kwargs: dict = field(default_factory=lambda: {"min_lr": 1e-5})
     lr_scheduler_type: str = "cosine_with_min_lr"
     max_prompt_length: int = 6200
@@ -26,7 +26,7 @@ class GRPOParams:
     num_epochs: int = 1
     overwrite_output_dir: bool = True
     resume_training_if_possible: bool = True
-    save_steps: float = 0.25
+    save_steps: float = 0.1
     seed: int = 42
     use_bf16: bool = True
     warmup_ratio: float = 0.05
@@ -43,7 +43,7 @@ class GenParams:
     vllm_server_host: str = "localhost"
     vllm_server_port: int = 8000
     vllm_server_timeout: int = 600
-    vllm_tensor_parallel_size: int = 8
+    vllm_tensor_parallel_size: int = 1
 
 
 @dataclass
@@ -53,11 +53,21 @@ class WandbParams:
 
 
 @dataclass
+class AblationParams:
+    reward_type: str = "correctness_only"  # Options: "correctness_only", "correctness_format", "correctness_format_length"
+    kl_penalty: str = "no"  # Options: "no", "static", "dynamic"
+    ref_model_syncup_alpha: float = 1.0  # 1.0 is a hard reset of the reference model, 0.0 is no syncup
+    ref_model_sync_steps: int = 100  # Number of steps to update KL penalty. Only relevant if kl_penalty is "dynamic"
+    loss_type: str = "dr_grpo"  # Options: "dr_grpo", "grpo", "dnpo"
+
+
+@dataclass
 class Config:
     data: Data = field(default_factory=Data)
     gen_params: GenParams = field(default_factory=GenParams)
     grpo_params: GRPOParams = field(default_factory=GRPOParams)
     wandb_params: WandbParams = field(default_factory=WandbParams)
+    ablation_params: AblationParams = field(default_factory=AblationParams)
 
 
 cs = ConfigStore.instance()
