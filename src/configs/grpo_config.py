c22ab4a39d4afe29156623a504e6acb2d31fcdfd
vatsal-kr <57312637+vatsal-kr@users.noreply.github.com>
2025-06-10 14:01:18 +0000

    Update configs

--- src/configs/grpo_config.py
+++ src/configs/grpo_config.py
@@ -11,16 +11,16 @@ class Data:
 
 @dataclass
 class GRPOParams:
-    batch_size: int = 4
+    batch_size: int = 32
     beta: float = 1e-3
     epsilon: float = 0.2
     epsilon_high: float = 0.4
     eval_batch_size: int = 16
-    gradient_accumulation_steps: int = 32
+    gradient_accumulation_steps: int = 1
     learning_rate: float = 1e-6
     logging_steps: int = 1
-    lr_scheduler_kwargs: dict = field(default_factory=lambda: {"min_lr": 1e-5})
-    lr_scheduler_type: str = "cosine_with_min_lr"
+    # lr_scheduler_kwargs: dict = field(default_factory=lambda: {"min_lr": 1e-5})
+    # lr_scheduler_type: str = "cosine_with_min_lr"
     max_prompt_length: int = 6200
     model_name: str = "CodeShield/sft-qwen3-0.6b"
     num_epochs: int = 1
@@ -37,24 +37,23 @@ class GRPOParams:
 class GenParams:
     max_completion_length: int = 8192
     num_generations: int = 8
-    temperature: float = 0.5
+    temperature: float = 0.8
     vllm_dtype: str = "bfloat16"
     vllm_gpu_memory_utilization: float = 0.4
     vllm_server_host: str = "localhost"
     vllm_server_port: int = 8000
     vllm_server_timeout: int = 600
-    vllm_tensor_parallel_size: int = 1
+    vllm_tensor_parallel_size: int = 4
 
 
 @dataclass
 class WandbParams:
     log_level: str = "info"
-    run_name: str = "grpo-test-run"
 
 
 @dataclass
 class AblationParams:
-    reward_type: str = "correctness_only"  # Options: "correctness_only", "correctness_format", "correctness_format_length"
+    reward_type: str = "correctness_only"  # Options: "correctness_only", "correctness_format", "correctness_format_length", "phi"
     kl_penalty: str = "no"  # Options: "no", "static", "dynamic"
     ref_model_syncup_alpha: float = 1.0  # 1.0 is a hard reset of the reference model, 0.0 is no syncup
     ref_model_sync_steps: int = 100  # Number of steps to update KL penalty. Only relevant if kl_penalty is "dynamic"
