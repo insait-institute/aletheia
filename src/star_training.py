46d5f073aa78efc5fde2a23e78901e8f3a761cc5
vatsal-kr <57312637+vatsal-kr@users.noreply.github.com>
2025-10-01 17:56:51 +0300

    create star and init genrm/dpo configs

---
+++ src/star_training.py
@@ -0,0 +1,270 @@
+import logging
+import os
+import pickle
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
+
+import hydra
+import torch
+from datasets import Dataset, load_dataset
+from omegaconf import OmegaConf
+from transformers import AutoTokenizer
+from trl import SFTConfig, SFTTrainer
+
+import wandb
+from cerebrm_prompts import STAR_PROMPT
+from cerebrm_rewards import extract_boxed_contents_list
+from configs.schema import Config
+from utils import get_generated_text, run_inference
+
+wandb.login()
+logging.basicConfig(level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+log.addHandler(ch)
+os.environ["WANDB_ENTITY"] = "CodeShield"
+os.environ["WANDB_PROJECT"] = "CerebRM-star"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+NUM_WORKERS = len(os.sched_getaffinity(0)) if hasattr(os, "sched_getaffinity") else 1
+
+
+@dataclass
+class Message:
+    role: str
+    content: str
+
+
+Prompt = List[Message]
+
+
+def is_valid_checkpoint(checkpoint_dir: str) -> bool:
+    if not os.path.isdir(checkpoint_dir):
+        return False
+
+    required_files = ["config.json", "tokenizer.json"]
+    model_files = ["pytorch_model.bin", "model.safetensors"]
+
+    # Check required config + tokenizer
+    for file in required_files:
+        if not os.path.isfile(os.path.join(checkpoint_dir, file)):
+            return False
+
+    # At least one model weight file must exist
+    if not any(os.path.isfile(os.path.join(checkpoint_dir, mf)) for mf in model_files):
+        return False
+
+    return True
+
+
+def _create_prompts(example, cfg: Config, hinted=False):
+    potential_answers = ["[[A]]", "[[B]]", "[[C]]", "[[D]]", "[[E]]"][: example["num_candidates"]]
+    candidates = [f"[CANDIDATE_{i[2]}]\n```{example['language']}\n{candidate}\n```\n[/CANDIDATE_{i[2]}]" for i, candidate in zip(potential_answers, example["candidates"])]
+    candidate_str = "\n\n".join(candidates)
+    if hinted:
+        candidate_str += f"\n\nHint: The correct answer is {example['chosen_answer']}"
+    example["prompt"] = [
+        {
+            "role": "user",
+            "content": STAR_PROMPT.format(
+                question=example["query"],
+                candidates=candidate_str,
+                valid_options=", ".join(potential_answers),
+            ).strip(),
+        },
+    ]
+    return example
+
+
+def generate_completions(cfg: Config, prompts: List[Prompt], model: str) -> List[str]:
+    # Sampling K responses from the current model
+    responses = run_inference(prompts, model, temperature=0.0, max_tokens=8192, n=1, tp_size=torch.cuda.device_count())
+    completions = get_generated_text(responses)
+    return [x[0] for x in completions]
+
+
+def score_completions(completions: List[str], ground_truths: List[str]) -> List[bool]:
+    # Scoring the K responses using a verifiable reward
+    model_answers = [extract_boxed_contents_list(x) for x in completions]
+    scored_completions = [model_ans == gt for model_ans, gt in zip(model_answers, ground_truths)]
+    return scored_completions
+
+
+def train_star_model(
+    cfg: Config,
+    model_path_episode: str,
+    stage3_data: Dataset,
+    wandb_run_name: str,
+    output_dir: str,
+) -> None:
+    config = SFTConfig(
+        model_init_kwargs={"attn_implementation": "kernels-community/flash-attn"},
+        output_dir=f"{output_dir}/intermediate_checkpoints",
+        overwrite_output_dir=cfg.star_params.overwrite_output_dir,
+        assistant_only_loss=True,
+        # Training parameters
+        bf16=cfg.star_params.use_bf16,
+        eval_strategy="no",
+        eval_steps=None,
+        gradient_accumulation_steps=cfg.star_params.gradient_accumulation_steps,
+        gradient_checkpointing=True,
+        gradient_checkpointing_kwargs={"use_reentrant": False},
+        learning_rate=cfg.star_params.learning_rate,
+        lr_scheduler_type=cfg.star_params.lr_scheduler_type,
+        lr_scheduler_kwargs=cfg.star_params.lr_scheduler_kwargs,
+        max_length=cfg.star_params.max_length,
+        num_train_epochs=cfg.star_params.num_epochs,
+        per_device_train_batch_size=cfg.star_params.batch_size,
+        per_device_eval_batch_size=cfg.star_params.batch_size,
+        seed=cfg.star_params.seed,
+        warmup_ratio=cfg.star_params.warmup_ratio,
+        weight_decay=cfg.star_params.weight_decay,
+        # Logging parameters
+        log_level=cfg.wandb_params.log_level,
+        log_on_each_node=True,
+        logging_steps=cfg.star_params.logging_steps,
+        load_best_model_at_end=False,
+        report_to="wandb",
+        run_name=wandb_run_name,
+        # Saving parameters
+        hub_model_id=f"CodeShield/{wandb_run_name}",
+        hub_private_repo=True,
+        hub_strategy="end",
+        save_strategy="steps",
+        save_steps=cfg.star_params.save_steps,
+        save_total_limit=cfg.star_params.save_total_limit,
+        # Data parameters
+        data_seed=cfg.star_params.seed,
+        dataloader_drop_last=True,
+        dataloader_num_workers=NUM_WORKERS,
+        dataset_num_proc=NUM_WORKERS,
+        remove_unused_columns=False,
+        use_liger_kernel=True,
+    )
+    trainer = SFTTrainer(model=model_path_episode, args=config, train_dataset=stage3_data)
+    trainer.train(resume_from_checkpoint=is_valid_checkpoint(config.output_dir))
+    trainer.push_to_hub()
+
+
+def _count_tokens(prompt: Prompt, tokenizer) -> int:
+    prompt = tokenizer.apply_chat_template(
+        prompt,
+        tokenize=False,
+        add_generation_prompt=False,
+    )
+    tokenized_prompt = tokenizer(prompt, padding=False, truncation=False)["input_ids"]
+    return len(tokenized_prompt)
+
+
+def create_messages_from_completions(prompts, completions):
+    messages = []
+    for prompt, completion in zip(prompts, completions):
+        if prompt[-1]["role"] == "assistant":
+            completion = prompt[-1]["content"] + completion
+            prompt = prompt[:-1]
+        messages.append(prompt + [{"role": "assistant", "content": completion}])
+    return messages
+
+
+def _by_tokens(example, max_tokens: int) -> bool:
+    return example["num_tokens"] <= max_tokens
+
+
+@hydra.main(version_base=None, config_path="configs", config_name="config")
+def main(cfg: Config):
+    ### Sanity checks
+    assert cfg.star_stage in [1, 2, 3], "Invalid star_stage. Must be 1 for generating and scoring completions, 2 for generating and scoring hinted completions, or 3 for training."
+    assert cfg.star_episode >= 0, "Invalid star_episode. Must be a non-negative integer."
+    output_dir = Path(f"{os.getenv('WORK')}/star_output/episode_{cfg.star_episode}")
+    if cfg.star_episode > 0:
+        for prev_ep in range(cfg.star_episode):
+            prev_dir = output_dir.parent / f"episode_{prev_ep}"
+            if not any(prev_dir.iterdir()):
+                raise ValueError(f"Previous episode directory {prev_dir} is empty. Please run stage 1, 2 and 3 for episode {prev_ep} before proceeding.")
+    if cfg.star_stage == 2:
+        if not (output_dir / "stage1_correct_messages.pkl").exists() or not (output_dir / "stage1_incorrect_indices.pkl").exists():
+            raise ValueError(f"Stage 1 files are missing in {output_dir}. Please run stage 1 for episode {cfg.star_episode} before proceeding.")
+    if cfg.star_stage == 3:
+        if not (output_dir / "correct_messages.pkl").exists():
+            raise ValueError(f"Stage 2 files are missing in {output_dir}. Please run stage 2 for episode {cfg.star_episode} before proceeding.")
+    ### End sanity checks
+    log.info(f"Config: {OmegaConf.to_yaml(OmegaConf.structured(cfg))}")
+    tokenizer = AutoTokenizer.from_pretrained(cfg.star_params.model_path)
+    train_data = load_dataset(cfg.data.train)["train"]
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+    model_short_name = cfg.star_params.model_path.split("/")[-1]
+    wandb_run_name = f"star_{model_short_name}_ep{cfg.star_episode}"
+    model_path_episode = cfg.star_params.model_path if cfg.star_episode == 0 else f"CodeShield/{wandb_run_name}"
+
+    train_data = train_data.map(_create_prompts, fn_kwargs={"cfg": cfg}, num_proc=NUM_WORKERS, desc="Creating prompts")
+    unhinted_prompts = list(train_data["prompt"])
+
+    log.info(f"Starting star episode {cfg.star_episode}")
+    if cfg.star_stage == 1:
+        log.info(f"Processing {len(unhinted_prompts)} prompts for stage 1")
+        stage1_completions = generate_completions(cfg, unhinted_prompts, model_path_episode)
+        log.info(f"Scoring {len(stage1_completions)} prompts for stage 1")
+        scored_completions = score_completions(stage1_completions, list(train_data["chosen_answer"]))
+        log.info(f"Number of correct responses in stage 1: {sum(scored_completions)} ({(sum(scored_completions) / len(scored_completions)) * 100:.2f}%)")
+
+        correct_indices = [i for i, score in enumerate(scored_completions) if score]
+        incorrect_prompts = [i for i, score in enumerate(scored_completions) if not score]
+
+        stage1_correct_messages = create_messages_from_completions(
+            [x for i, x in enumerate(unhinted_prompts) if i in correct_indices], [x for i, x in enumerate(stage1_completions) if i in correct_indices]
+        )
+        stage2_indices = [i for i in incorrect_prompts]
+
+        with open(output_dir / "stage1_correct_messages.pkl", "wb") as f:
+            pickle.dump(stage1_correct_messages, f)
+        with open(output_dir / "stage1_incorrect_indices.pkl", "wb") as f:
+            pickle.dump(stage2_indices, f)
+        log.info(f"Completed Stage 1 of star episode {cfg.star_episode}")
+
+    elif cfg.star_stage == 2:
+        train_data = train_data.map(_create_prompts, fn_kwargs={"cfg": cfg, "hinted": True}, num_proc=NUM_WORKERS, desc="Creating prompts")
+        hinted_prompts = list(train_data["prompt"])
+        with open(output_dir / "stage1_incorrect_indices.pkl", "rb") as f:
+            stage2_indices = pickle.load(f)
+        with open(output_dir / "stage1_correct_messages.pkl", "rb") as f:
+            stage1_correct_messages = pickle.load(f)
+
+        stage2_prompts = [hinted_prompts[i] for i in stage2_indices]
+        stage2_answers = list(train_data.select(stage2_indices)["chosen_answer"])
+
+        log.info(f"Processing {len(stage2_prompts)} prompts for stage 2")
+        stage2_completions = generate_completions(cfg, stage2_prompts, model_path_episode)
+        log.info(f"Scoring {len(stage2_completions)} prompts for stage 2")
+        scored_completions = score_completions(stage2_completions, stage2_answers)
+
+        correct_indices = [i for i, score in enumerate(scored_completions) if score]
+        log.info(f"Number of correct responses in stage 2: {sum(scored_completions)} ({(sum(scored_completions) / len(scored_completions)) * 100:.2f}%)")
+        stage2_correct_messages = create_messages_from_completions(
+            [x for i, x in enumerate(unhinted_prompts) if i in correct_indices], [x for i, x in enumerate(stage2_completions) if i in correct_indices]
+        )
+        correct_messages = stage1_correct_messages + stage2_correct_messages
+        with open(output_dir / "correct_messages.pkl", "wb") as f:
+            pickle.dump(correct_messages, f)
+    else:
+        with open(output_dir / "correct_messages.pkl", "rb") as f:
+            stage3_data = pickle.load(f)
+        stage3_data = Dataset.from_dict({"messages": stage3_data})
+        stage3_data = stage3_data.map(_count_tokens, fn_kwargs={"tokenizer": tokenizer}, num_proc=NUM_WORKERS, desc="Counting tokens")
+        stage3_data = stage3_data.filter(_by_tokens, fn_kwargs={"max_tokens": cfg.star_params.max_length}, num_proc=NUM_WORKERS, desc="Filtering long sequences")
+
+        log.info(f"Training {cfg.star_params.model_path} on {len(stage3_data)} examples in stage three")
+        train_star_model(cfg, cfg.star_params.model_path, stage3_data, wandb_run_name, output_dir)
+        log.info(f"Completed Stage 2 of star episode {cfg.star_episode}")
+
+
+if __name__ == "__main__":
+    """
+    STaR stages: 
+    Stage 1: Sample 1 response from the current model for each prompt in the training set and score all the responses
+    Stage 2: Sample and score responses with prompts augmented with a hint for the incorrect prompts in stage 1
+    Stage 3: Train on the final dataset of correct responses from stage 1 and 2
+    """
+    main()
